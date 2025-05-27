148bf4687524a43f457dd443976e165cc9904a22
LK152 <wuluke0@gmail.com>
2025-05-28 02:19:57 +0800

    f

--- client/src/components/FloatingMenu.tsx
+++ client/src/components/FloatingMenu.tsx
@@ -6,28 +6,42 @@ const FloatingMenu = ({
 	onRouteClick,
 	onAboutClick,
 	onSDGsClick,
-    userName
+	userName,
 }: {
 	onRouteClick: () => void;
 	onAboutClick: () => void;
 	onSDGsClick: () => void;
-    userName: string | null;
+	userName: string | null;
 }) => {
 	const [open, setOpen] = useState(false);
-    const nav = useNavigate();
+	const nav = useNavigate();
 
 	return (
 		<>
 			{/* 固定右上角按鈕 */}
-			<div className="floating-button">
-				<button className="fab-main" onClick={() => setOpen(!open)}>
+			<div className='floating-button'>
+				<button className='fab-main' onClick={() => setOpen(!open)}>
 					{open ? '✕' : '≡'}
 				</button>
 			</div>
 
 			{/* 展開選單本體（絕對定位，不會影響上方按鈕） */}
 			<div className={`fab-options ${open ? 'show' : ''}`}>
-                {userName ? <button>{`Hello, ${userName}`}</button> : <button onClick={() => nav('/login')}>登入</button>}
+				{userName ? (
+					<>
+						<button>{`Hello, ${userName}`}</button>
+						<button
+							onClick={() => {
+								localStorage.clear();
+								nav('/');
+							}}
+						>
+							登出
+						</button>
+					</>
+				) : (
+					<button onClick={() => nav('/login')}>登入</button>
+				)}
 				<button onClick={onRouteClick}>🧭 設定路線</button>
 				<button onClick={onAboutClick}>ℹ️ 關於我們</button>
 				<button onClick={onSDGsClick}>♻️ SDGs 宣導</button>
