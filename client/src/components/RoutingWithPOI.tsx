607bb1b920ed5dc1afec76fcb01f17aa872b946a
LK152 <wuluke0@gmail.com>
2025-05-28 03:06:06 +0800

    f

--- client/src/components/RoutingWithPOI.tsx
+++ client/src/components/RoutingWithPOI.tsx
@@ -93,7 +93,7 @@ const RoutingWithPOI: React.FC<RoutingWithPOIProps> = ({ spots, routeMode }) => 
           <Popup>
             <h3>{spot.name}</h3>
             <img
-              src={`http://lukewu.site:8088/images/spots/${spot.image}`}
+              src={`https://nukserver.xn--hrr.tw/images/spots/${spot.image}`}
               alt={spot.name}
               style={{ width: '100px', borderRadius: '8px' }}
             />
